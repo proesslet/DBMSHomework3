396fc83971c1f45023c9ad5c41a34e768103e67c
Preston Roesslet <proesslet@gmail.com>
2024-10-10 15:21:27 -0500

    problem 2 sql done

--- problem2.sql
+++ problem2.sql
@@ -1,3 +1,78 @@
-CREATE PROCEDURE InsertFaculty @Fid @Name NVARCHAR(250), 
+IF OBJECT_ID('InsertFacultyByDept', 'P') IS NOT NULL
+BEGIN
+    DROP PROCEDURE InsertFacultyByDept;
+END;
+GO
+
+IF OBJECT_ID('InsertFacultyWhereNotDept', 'P') IS NOT NULL
+BEGIN
+    DROP PROCEDURE InsertFacultyWhereNotDept;
+END;
+GO
+
+IF OBJECT_ID('SelectAllFaculty', 'P') IS NOT NULL
+BEGIN
+    DROP PROCEDURE SelectAllFaculty;
+END;
+GO
+
+-- Procedure 1
+CREATE PROCEDURE InsertFacultyByDept
+    @fid INT,
+    @fname VARCHAR(250),
+    @deptid INT
 AS
-INSERT INTO Faculty 
+BEGIN
+    DECLARE @avg_salary DECIMAL(10, 2);
+    DECLARE @new_salary DECIMAL(10, 2);
+
+    -- Calculate the average salary for the department
+    SELECT @avg_salary = AVG(salary)
+    FROM Faculty
+    WHERE deptid = @deptid;
+
+    -- Calculate the new salary according to the logic
+    SET @new_salary = CASE
+        WHEN @avg_salary > 50000 THEN @avg_salary * 0.9
+        WHEN @avg_salary < 30000 THEN @avg_salary
+        ELSE @avg_salary * 0.8  -- Adjusted to 80% for averages between 30,000 and 50,000
+    END;
+
+    -- Insert the new faculty member
+    INSERT INTO Faculty (fid, fname, deptid, salary)
+    VALUES (@fid, @fname, @deptid, @new_salary);
+END;
+GO
+
+-- Procedure 2
+CREATE PROCEDURE InsertFacultyWhereNotDept
+@fid INTEGER,
+@fname VARCHAR(250),
+@deptid INTEGER,
+@notdept INTEGER
+AS
+BEGIN
+    DECLARE @avg_salary DECIMAL(10, 2);
+
+    SELECT @avg_salary = AVG(salary)
+    FROM Faculty
+    WHERE deptid <> @notdept;
+
+    -- Insert the new faculty member
+    INSERT INTO Faculty (fid, fname, deptid, salary)
+    VALUES (@fid, @fname, @deptid, @avg_salary);
+
+END;
+GO
+
+CREATE PROCEDURE SelectAllFaculty
+AS
+BEGIN
+    SELECT *
+      FROM Faculty;
+
+END;
+GO
+
+EXEC SelectAllFaculty
+GO
